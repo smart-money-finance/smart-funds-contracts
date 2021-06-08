1863adadbff2734b5547587d9cf8b63d94e79c9d
John Moffitt <39016062+whatsyourgithub@users.noreply.github.com>
2021-06-08 16:35:20 -0700

    add rinkeby support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,10 @@ import { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
   networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: { mnemonic: process.env.MNEMONIC },
+    },
     goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
       accounts: { mnemonic: process.env.MNEMONIC },
