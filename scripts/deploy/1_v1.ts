b77d65cb834b1127b023afdf6843d91f3e9f3754
shayanja <FA28080D091ACED0>
2021-07-23 02:08:30 +0300

    Update usdc token address rinkeby

--- scripts/deploy/1_v1.ts
+++ scripts/deploy/1_v1.ts
@@ -8,7 +8,7 @@ async function main() {
   const factory = await SmartFundFactory.deploy(
     masterFundLibrary.address,
     {
-      rinkeby: '0x2A20b20689f856478D864241Df8Ae9063c34e701',
+      rinkeby: '0xe3f8c202317F4f273BAf2097DD5bCBd3eBBE9B85',
       goerli: '0xde637d4c445ca2aae8f782ffac8d2971b93a4998',
       mainnet: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
     }[network.name], // USDC address
